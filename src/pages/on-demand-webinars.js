398e228f8d350b8260671f2550e6d0b0d9df5a2a
vedsu <90701239+vedsu@users.noreply.github.com>
2026-07-10 17:47:54 +0530

    Rename 'On-Demand Webinars' to 'Recorded Webinars'

--- src/pages/on-demand-webinars.js
+++ src/pages/on-demand-webinars.js
@@ -46,10 +46,10 @@ function OndemansWebinars() {
 			<div className="container">
 				<div className="col-lg-10 offset-lg-1 text-center">
 					<div className="section-top-title wow fadeInRight" data-wow-duration="1s" data-wow-delay="0.3s" data-wow-offset="0">
-						<h1>On-Demand Webinars</h1>
+						<h1>Recorded Webinars</h1>
 						<ul>
 							<li><a href="/">Home</a></li>
-							<li> / Browse Courses / On-Demand Webinars</li>
+							<li> / Browse Courses / Recorded Webinars</li>
 						</ul>
 					</div>
 				</div>
