c1699cfba22ba19d4b763c8545473f76b7508893
vedsu <90701239+vedsu@users.noreply.github.com>
2026-07-17 15:45:38 +0530

    Update speaker ID format in WebinarDetails

--- src/pages/WebinarDetails.js
+++ src/pages/WebinarDetails.js
@@ -933,8 +933,8 @@ button.btn_one.webinar-details {
               Webinar ID
             </small>
 
-          <strong>
-  {String(webinar.speaker_id).replace(/[A-Za-z]/g, "")}
+         <strong>
+  {`${String(webinar.speaker_id).replace(/[A-Za-z_]/g, "")}00${webinar.id}`}
 </strong>
 
           </div>
